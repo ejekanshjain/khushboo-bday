565f98912beeddbc7c015cbe88ef628bf13d1dac
ejekanshjain <ejekanshjain@gmail.com>
2020-05-13 22:37:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(port, () => console.log(`Server Started on port ${port}...`))
